2f367ff834581fdf247b51ba977381a8ccb82fff
Vladislav Shesternin <54269161+Vladislav-Shesternin@users.noreply.github.com>
2025-04-13 12:46:05 +0200

    new version

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,26 @@
+const CACHE_NAME = 'uxo-cache-v2'; // ← змінюй версію при кожному оновленні
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('uxo-cache-v1').then(function(cache) {
+    caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
         './',
         './index.html'
       ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(name) {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
     })
   );
 });
